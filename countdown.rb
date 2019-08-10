143e44ee2364372d2e0df5337c732b10c22e6aed
Michael Lynch <michael@michaelrlynch.com>
2019-08-10 03:57:35 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown(secs)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(secs)
+  while secs > 0
+    puts "#{secs} SECOND(S)!"
+    secs -= 1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
+end
